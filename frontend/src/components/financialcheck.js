64d5bebca1b0c24997206cb147891a3404cde36d
sherry <25100169@lums.edu.pk>
2024-04-18 01:01:58 +0500

    undo breaking change

--- frontend/src/components/financialcheck.js
+++ frontend/src/components/financialcheck.js
@@ -54,49 +54,48 @@ const FinancialCheck = () => {
     fetchProfile();
   }, [email, role]);
 
-  window.onload = function() {
-    const barGraph = (info) => {
-      const graphData = {
-        labels: ['Salary', 'Bonuses', 'Commissions', 'Expenses'],
-        datasets: [
-          {
-            label: 'Financial Information',
-            data: [info.salary, info.bonuses, info.commissions, info.expenses],
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)'
-            ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)'
-            ],
-            borderWidth: 4
-          }
-        ]
-      };
-      
-      const ctx = document.getElementById('myChart').getContext('2d');
-      const myChart = new Chart(ctx, {
-        type: 'bar',
-        data: graphData,
-        options: {
-          scales: {
-            yAxes: [{
-              ticks: {
-                beginAtZero: true
-              }
-            }]
-          }
-        }
-      });
-    };
+  const barGraph = (info) => {
+  // window.alert('info: ' + JSON.stringify(info));
+  const graphData = {
+    labels: ['Salary', 'Bonuses', 'Commissions', 'Expenses'],
+    datasets: [
+      {
+        label: 'Financial Information',
+        data: [info.salary, info.bonuses, info.commissions, info.expenses],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)'
+        ],
+        borderWidth: 4
+      }
+    ]
   };
+  
+  const ctx = document.getElementById('myChart').getContext('2d');
+  const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: graphData,
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
+};
 
 const handleGoBack = () => {
   if(passedThat === null || passedThat === undefined || passedThat.imfrom === null || passedThat.imfrom === undefined)
